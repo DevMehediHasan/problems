c4b255929c1515abd6fab38f39a214d44deecbd6
EngMehed <mehedhasan87@gmail.com>
2021-11-04 01:27:57 +0600

    conditional problem solving

--- index.js
+++ index.js
@@ -47,3 +47,50 @@ if(m>n && m>o && m>p && m>q) {
 } else {
     console.log(a);
 }
+
+// Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen.
+for ( var i = 0; i <= 15; i++) {
+    if(i % 2 === 0){
+        console.log(i + ' is even');
+    } else {
+        console.log(i + ' is odd');
+    }
+}
+
+//Write a JavaScript program which compute, the average marks of the following students Then, this average is used to determine the corresponding grade.
+var students = [['David', 80], ['Vinoth', 77], ['Divya', 88], ['Ishitha', 95], ['Thomas', 68]];
+
+var Avgmarks = 0;
+
+for (var i=0; i < students.length; i++) {
+        Avgmarks += students[i][1];
+        var avg = (Avgmarks/students.length);
+}
+console.log(Avgmarks);
+
+console.log("Average grade: " + avg);
+
+        if (avg < 60){
+          console.log("Grade : F");      
+          } 
+        else if (avg < 70) {
+                console.log("Grade : D"); 
+                  } 
+        else if (avg < 80) 
+             {
+                console.log("Grade : C"); 
+        } else if (avg < 90) {
+                console.log("Grade : B"); 
+        } else if (avg < 100) {
+                console.log("Grade : A"); 
+}
+
+// Write a JavaScript program to construct the following pattern, using a nested for loop.
+var mk = 10
+for ( i = 1; i <=mk; i++ ) {
+        var result = ''
+    for( j=1; j <=i; j++) {
+        result += '*'
+    }
+    console.log(result);
+}
